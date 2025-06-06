58b18c389b917ec9ee7218ecf40d06773ea9c321
Shashwat Panigrahi <shashwat@Mac.lan>
2025-06-06 09:47:55 -0400

    First commit

---
+++ src/main/java/com/cookbase/cookbaseapi/CookbaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.cookbase.cookbaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookbaseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookbaseApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cookbase/cookbaseapi/CookbaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookbase.cookbaseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookbaseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
